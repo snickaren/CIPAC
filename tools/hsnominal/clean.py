e71c6f783f67ef8fdf26216cb46f848b741e00a3
peterk <peter@peterkrantz.se>
2017-01-07 22:58:33 +0100

    HS Nominal clean and OCR script

---
+++ tools/hsnominal/clean.py
@@ -0,0 +1,65 @@
+from wand.drawing import Drawing
+from wand.image import Image
+from wand.color import Color
+from wand.api import library
+import sys
+from wand.display import display
+import ctypes
+import subprocess
+
+library.MagickSetImageFuzz.argtypes = (ctypes.c_void_p, ctypes.c_double)
+
+def clean(imagepath):
+    """Batch clean scanned HS nominal catalog card image before OCR."""
+
+    with Image(filename=imagepath) as img:
+
+        # Hole coordinates
+        delta = 30
+        hole_x = int(img.width/2) - int(delta/2)
+        hole_y = int(img.height*0.88)
+
+        avgcol = None
+        imgcol = img.clone()
+        with Color("black") as black:
+
+            # Find average color (for hole and corner fill)
+            imgcol.transparent_color(black, alpha=0.0, fuzz=50)
+            imgcol.resize(1, 1)
+            avgcol = imgcol[0,0]
+
+            command = ["convert"]
+            command.append("'%s'" % imagepath)
+
+            # Add border
+            command.append("-bordercolor black -border 10x10")
+
+            # Fill border and corners with white
+            command.append("-fuzz 20% -fill white -draw 'color 5,5 floodfill'")
+
+            # Fill hole at several points to catch stripe
+            for delta in [0, 10, 20, 40]:
+                command.append("-fuzz 20% -fill white -draw")
+                command.append(""" "color %s,%s floodfill" """ % (hole_x + delta, hole_y))
+
+            # add card average color to previously filled areas
+            command.append("""\( -clone 0 -fill "%s" -colorize 100 \)""" % avgcol)
+            command.append("""\( -clone 0 -negate -threshold 1% -negate -morphology Dilate octagon:10 -blur 0x8 \)""")
+            command.append("-compose over")
+            command.append("-composite")
+
+            # make grayscale
+            command.append("-colorspace gray")
+
+            command.append("'%s.clean8.jpg'" % imagepath)
+
+            subprocess.run(" ".join(command), shell=True, stdout=subprocess.PIPE)
+
+            #cleanup with Textcleaner (see http://www.fmwconcepts.com/imagemagick/textcleaner/)
+            command = "/path/to/textcleaner -e normalize -f 20 -o 5 '%s.clean8.jpg' '%s.clean9.jpg'" % (imagepath, imagepath)
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    clean(path)
--- tools/hsnominal/hsnominal-ocr.sh
+++ tools/hsnominal/hsnominal-ocr.sh
@@ -3,15 +3,17 @@
 
 # Katalogen består av digitaliserade filer för fram- och baksida av alla kort.
 
-# Skriptet använder Tesseract 4 för OCR.
+# Skriptet använder Tesseract 4 för OCR (version HEAD-6f83ba0).
 
 # Sätt TESSDATA_PREFIX till den katalog där språkfiler mm för tesseract finns.
 # Resultat sparas i samma katalog som bildfilen
 
+# Kör med parallel --eta -a hsnominal-files.txt ./hsnominal/hsnominal-ocr.sh
+
 export TESSDATA_PREFIX=/path/to/tesseract/tessdata/parent/folder
 
-if [ ! -f "$1.txt" ]; then
-  tesseract "$1.clean.jpg" "$1" --oem 2 --psm 4 -l swe+lat hsnominal
+if [ ! -f "$1.clean9.txt" ]; then
+  tesseract "$1.clean9.jpg" "$1.clean9" --oem 2 --psm 6 -l swe+lat hsnominal
 else
-  echo "Skipping $1.txt"
+  echo "Skipping $1.clean9.txt"
 fi
