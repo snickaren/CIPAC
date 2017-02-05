26516a8521c0e5be17baf03a63b835bf25265317
peterk <peter@peterkrantz.se>
2017-02-05 13:35:51 +0100

    Pagination fix

--- webapp/kortkatalogen/ullas/urls.py
+++ webapp/kortkatalogen/ullas/urls.py
@@ -4,5 +4,5 @@ from ullas import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^(?P<card_catalog_sequence_number>[0-9]+)/$', views.browse, name='ullacardurl'),
+    url(r'^(?P<card_catalog_sequence_number>[0-9]+)/$', views.browse, name='cardurl'),
 ]
